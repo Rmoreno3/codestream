8fa700ca2743e5b46e0a229dc4a3e6cf1fdaa6e9
Reynaldo Moreno <73906495+Rmoreno3@users.noreply.github.com>
2021-08-09 00:01:16 -0400

    Translate email confirmation to Spanish (#1)

--- shared/ui/translations/es.js
+++ shared/ui/translations/es.js
@@ -1,4 +1,5 @@
 export default {
+	"confirmation.checkEmail": "Verifica tu correo electronico",
 	"signUp.passwordReset": "Reestablecer contraseña",
 	"signUp.passwordResetEnterEmail":
 		"Ingrese su dirección de correo electrónico y le enviaremos un enlace para restablecer su contraseña."
